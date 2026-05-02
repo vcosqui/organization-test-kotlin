4fb8d4e800327bc47dfa7459c04905ceae6adeea
Victor Cosqui <victor.cosqui@gmail.com>
2026-05-02 07:04:50 +0200

    remove the redundant @Autowired and the annotation import aliases

--- src/main/kotlin/com/company/organization/rest/OrganizationController.kt
+++ src/main/kotlin/com/company/organization/rest/OrganizationController.kt
@@ -1,26 +1,25 @@
 package com.company.organization.rest
 
 import com.company.organization.domain.port.OrganizationUseCase
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.http.MediaType.APPLICATION_JSON_VALUE as APPLICATION_JSON
-import org.springframework.web.bind.annotation.GetMapping as GET
-import org.springframework.web.bind.annotation.PostMapping as POST
 
 @RestController
-class OrganizationController(@Autowired val organization: OrganizationUseCase) {
+class OrganizationController(val organization: OrganizationUseCase) {
 
-    @POST("/organization", consumes = [APPLICATION_JSON])
+    @PostMapping("/organization", consumes = [APPLICATION_JSON_VALUE])
     fun setOrganization(@RequestBody employeesMap: Map<String, String>) = organization.addEmployees(employeesMap)
 
-    @GET("/organization", produces = [APPLICATION_JSON])
+    @GetMapping("/organization", produces = [APPLICATION_JSON_VALUE])
     fun getOrganization(): Map<String, Any> {
         return topDown(listOfNotNull(organization.getRootEmployee()))
     }
 
-    @GET("/organization/employee/{employeeName}/management", produces = [APPLICATION_JSON])
+    @GetMapping("/organization/employee/{employeeName}/management", produces = [APPLICATION_JSON_VALUE])
     fun getEmployeeManagement(@PathVariable employeeName: String): Map<String, Any> {
         return upstreamHierarchy(organization.getEmployee(employeeName))
     }
